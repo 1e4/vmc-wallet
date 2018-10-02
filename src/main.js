293e84a317af140118ec2464f0d49d0c7d34e80d
Ian <hello@ian.pe>
2018-10-02 14:52:44 +0200

    remove redundant code

--- src/main.js
+++ src/main.js
@@ -5,17 +5,7 @@ import io from 'socket.io-client'
 
 Vue.config.productionTip = false;
 
-window.$socket = io('http://35.204.157.208:8000/root');
-
 window.$user = io('http://35.204.157.208:8000/user');
-
-window.$socket.on("connect", () => {
-    console.log("Connected");
-});
-
-window.$socket.on("event", (data) => {
-    console.log(data);
-});
 
 new Vue({
     router,
